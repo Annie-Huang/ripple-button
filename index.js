6ab05bfe70dd44aeaf483f2d16ded3e3f6c18394
Annie Huang <annie.hn@gmail.com>
2022-06-11 12:25:15 +1000

    When the animiation finished, remove the .pulse class.

--- index.js
+++ index.js
@@ -15,4 +15,10 @@ function mousePositionToCustomProp(event, element) {
 rippleButton.addEventListener('click', (e) => {
   mousePositionToCustomProp(e, rippleButton);
   rippleButton.classList.add('pulse'); // javascript cannot really grep the sudo element
+  rippleButton.addEventListener('animationend', () => {
+    rippleButton.classList.remove('pulse');
+  });
+
+  // Not sure why Kevin doesn't remove it the event listener, wouldn't it be creating a new one each time we click?
+  rippleButton.removeEventListener('animationend');
 });
